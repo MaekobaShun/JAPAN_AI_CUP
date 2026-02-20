611b240cd09570f4d77f48c3eaecd825517223d4
MaekobaShun <maekoba.shun.job@gmail.com>
2026-02-20 10:13:26 +0900

    使わなくてok

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+JAPAN AI CUP プロジェクトのソースコード
+"""
+
+__version__ = "0.1.0"
+
+# from src import ○○と書くことによって、インポートが楽になる
+# from src.models.train import Trainerがfrom src import Trainerで良くなる
+# ただ、使わなくても大丈夫
